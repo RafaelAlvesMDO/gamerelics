ec81f65c53e34c03b59f14d60baf812c42a03d89
Rafael Alves <rafaelalvesmdodev@gmail.com>
2026-02-27 21:39:06 -0300

    Feat: added themes folder where will be storaged the system colors palette.

---
+++ src/themes/colorsPalette.ts
@@ -0,0 +1,10 @@
+// COLOR PALETTE USED DURING THE WHOLE APP
+
+export const colorsPalette = {
+  background: "#0f0f0f",
+  textPrimary: "#FFFFFF",
+  textSecondary: "#B3B3B3",
+  button: "#4DA3FF",
+  error: "#FF4D4D",
+  success: "#4CAF50",
+};
