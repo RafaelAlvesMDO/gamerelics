f1e324938d3e039aad7ea01c53046b16e0bc7c50
Rafael Alves <rafaelalvesmdodev@gmail.com>
2026-03-03 19:36:10 -0300

    Update: Removed button and input styles, that was moved to his own components files

---
+++ src/styles/authStyleSheet.ts
@@ -0,0 +1,95 @@
+import { colorsPalette } from '@/src/themes/colorsPalette';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: colorsPalette.background,
+  },
+
+  topContainer: {
+    height: 220,
+    overflow: "hidden",
+  },
+
+  backgroundImage: {
+    ...StyleSheet.absoluteFillObject,
+    opacity: 0.3,
+  },
+
+  gradient: {
+    ...StyleSheet.absoluteFillObject,
+  },
+
+  topContent: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingBottom: 24,
+  },
+
+  iconBox: {
+    width: 64,
+    height: 64,
+    borderRadius: 16,
+    backgroundColor: "#7c3aed1a", // CHANGE TO SECONDARY COLOR [TO DECIDE] 
+    borderWidth: 1,
+    borderColor: "#7c3aed4d", // CHANGE TO SECONDARY COLOR [TO DECIDE] 
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+
+  topTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: colorsPalette.textPrimary,
+  },
+
+  topSubtitle: {
+    fontSize: 14,
+    color: colorsPalette.textSecondary,
+    marginTop: 4,
+  },
+
+  form: {
+    paddingHorizontal: 24,
+    paddingTop: 24,
+  },
+
+  formTitle: {
+    fontSize: 22,
+    fontWeight: "600",
+    color: colorsPalette.textPrimary,
+    marginBottom: 4,
+  },
+
+  formSubtitle: {
+    fontSize: 14,
+    color: colorsPalette.textSecondary,
+    marginBottom: 24,
+  },
+
+  forgot: {
+    color: colorsPalette.primary,
+    alignSelf: "flex-end",
+    marginBottom: 12,
+  },
+
+  authTextRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 30,
+    marginBottom: 40,
+  },
+
+  authText: {
+    color: "#a1a1aa",
+    marginRight: 4,
+  },
+
+  authLink: {
+    color: colorsPalette.primary,
+    fontWeight: "600",
+  },
+});
