08a4015f08e9402895db1543ce7f401078dc053d
Rafael Alves <rafaelalvesmdodev@gmail.com>
2026-03-03 19:33:59 -0300

    Feat: Created button component to make reusable and practical

---
+++ src/components/button.tsx
@@ -0,0 +1,51 @@
+import { colorsPalette } from '@/src/themes/colorsPalette';
+import { GestureResponderEvent, StyleSheet, Text, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
+
+export interface ButtonProps {
+  title: string;
+  backgroundColor?: string;
+  style?: ViewStyle;
+  textStyle?: TextStyle;
+  onPress?: (event: GestureResponderEvent) => void;
+}
+
+export function Button({
+  title,
+  backgroundColor = colorsPalette.primary,
+  style,
+  textStyle,
+  onPress,
+}: ButtonProps) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View
+        style={[
+          styles.buttonContainer,
+          { backgroundColor },
+          style,
+        ]}
+      >
+        <Text style={[styles.buttonText, textStyle]}>
+          {title}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        height: 48,
+        backgroundColor: colorsPalette.primary,
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 8,
+    },
+    
+    buttonText: {
+        color: colorsPalette.textPrimary,
+        fontWeight: "600",
+        fontSize: 16,
+    },
+})
