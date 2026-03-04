2044c70919db058f3e2936129fa761d2dea1a714
Rafael Alves <rafaelalvesmdodev@gmail.com>
2026-03-03 21:17:41 -0300

    Update: changed formTitle fontSize from 22 to 24 making Title font sizes equal for all

--- src/styles/authStyleSheet.ts
+++ src/styles/authStyleSheet.ts
@@ -58,7 +58,7 @@ export const styles = StyleSheet.create({
   },
 
   formTitle: {
-    fontSize: 22,
+    fontSize: 24,
     fontWeight: "600",
     color: colorsPalette.textPrimary,
     marginBottom: 4,
