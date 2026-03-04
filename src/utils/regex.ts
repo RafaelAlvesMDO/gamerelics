d31788824d67383456f34adc7cff006760dcdd68
Rafael Alves <rafaelalvesmdodev@gmail.com>
2026-03-04 00:51:03 -0300

    Feat: Added regex types that are used with Yup for more specific validation rules

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,2 +1,5 @@
 export const passwordRegex =
-  /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+    /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+
+export const nameRegex =
+    /^[A-Za-zÀ-ÖØ-öø-ÿ\s-]+$/;
