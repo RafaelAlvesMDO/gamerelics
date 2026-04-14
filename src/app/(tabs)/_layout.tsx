bec911832e44a21e24544c498cf1b5201b799985
Rafael Alves <rafaelalvesmdodev@gmail.com>
2026-04-14 17:04:13 -0300

    Feat: Added navigation to game section, forms and details

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -40,6 +40,28 @@ export default function Layout() {
             ),
           }}
         />
+
+        <Tabs.Screen
+          name="gameSection"
+          options={{
+            href: null,
+          }}
+        />
+
+        <Tabs.Screen
+          name="gameForms"
+          options={{
+            href: null,
+          }}
+        />
+
+        <Tabs.Screen
+          name="gameDetails"
+          options={{
+            href: null,
+          }}
+        />
+
       </Tabs>
     </SafeAreaView>
   );
